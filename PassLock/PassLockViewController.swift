a162a3d2a05a34d52484bfc22a0f94520697ba09
edison <ll.buptee@gmail.com>
2016-09-20 10:54:12 +0800

    Add PassLockViewController & Storyboard

---
+++ PassLock/PassLockViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PassLockViewController.swift
+//  PassLockDemo
+//
+//  Created by edison on 9/20/16.
+//  Copyright © 2016 NSCodeMonkey. All rights reserved.
+//
+
+import UIKit
+
+enum PassLockType {
+  case SetPassword
+  case ChangePassword
+  case RemovePassword
+}
+
+public class PassLockViewController: UIViewController {
+
+}
