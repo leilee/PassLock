21bc3cbfbb5413bc0ee74699892664eb50c237d5
edison <ll.buptee@gmail.com>
2016-09-20 12:31:06 +0800

    Add Config.swift

---
+++ PassLock/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  PassLockDemo
+//
+//  Created by edison on 9/20/16.
+//  Copyright © 2016 NSCodeMonkey. All rights reserved.
+//
+
+import Foundation
+
+public enum PassLockType {
+  case SetPassword
+  case ChangePassword
+  case RemovePassword
+}
