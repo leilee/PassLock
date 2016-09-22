3e140a080938e554bc4c2447613bd180868b3446
edison <ll.buptee@gmail.com>
2016-09-22 11:28:01 +0800

    修改「删除密码」状态中有歧义的部分

--- PassLock/PassLockViewController+States.swift
+++ PassLock/PassLockViewController+States.swift
@@ -19,7 +19,7 @@ enum PassLockEvent {
 extension PassLockType {
   func nextEvent(x x: Password?, y: Password?, with state: PassLockState) -> PassLockEvent {
     switch (self, state) {
-    case (.SetPassword, .Confirm), (.RemovePassword, .Input):
+    case (.SetPassword, .Confirm), (.RemovePassword, .Confirm):
       return x == y ? .Valid : .Invalid
     default:
       return .Valid
@@ -68,31 +68,31 @@ extension PassLockViewController {
   }
 
   func removePasswordStateMachine() -> StateMachine<PassLockState, PassLockEvent, Password> {
-    return StateMachine<PassLockState, PassLockEvent, Password>(initialState: .Input) { [weak self] (state, event) in
+    return StateMachine<PassLockState, PassLockEvent, Password>(initialState: .Confirm) { [weak self] (state, event) in
       switch (state, event) {
-      case (.Input, .Valid): return (.Done, {  _, _, _ in
-        // input => done
+      case (.Confirm, .Valid): return (.Done, {  _, _, _ in
+        // confirm => done
         guard let strongSelf = self else {
           return
         }
         strongSelf.descriptionLabel.hidden = true
         strongSelf.delegate?.passLockController(strongSelf, removePassLock: true)
       })
-      case (.Input, .Invalid):
+      case (.Confirm, .Invalid):
         guard let strongSelf = self else {
           return nil
         }
         strongSelf.retryCount += 1
         if strongSelf.retryCount >= strongSelf.config.retryCount {
-          // failure
+          // exceed retry count, failure
           return (.Done, { _, _, _ in
             strongSelf.descriptionLabel.hidden = true
             strongSelf.delegate?.passLockController(strongSelf, removePassLock: false)
           })
         } else {
           // retry
-          return (.Input, { _, _, _ in
-            strongSelf.passwordInputView.shake({ 
+          return (.Confirm, { _, _, _ in
+            strongSelf.passwordInputView.shake({
               strongSelf.passwordInputView.clear()
               strongSelf.descriptionLabel.hidden = false
               strongSelf.descriptionLabel.text = "密码不匹配, 您还有\(strongSelf.config.retryCount - strongSelf.retryCount)次尝试机会"
